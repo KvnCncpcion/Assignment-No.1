bee7b256366314f06a71c51a897c775cc9870b02
KvnCncpcion <116430573+KvnCncpcion@users.noreply.github.com>
2022-10-22 18:10:21 +0800

    Add files via upload

---
+++ NAMEINASTERISK.py
@@ -0,0 +1,11 @@
+print("***************PROGRAMMED BY**************")
+print("********Kevin Joseph G. Concepcion********")
+print("*****************BSCOE 2-2****************")
+print("**********Sir Danilo Madrigalejos*********\n")
+
+print("**         **   ********   **    **")
+print(" **       **       **      ***   **")
+print("  **     **        **      ** *  **")
+print("   **   **         **      **  * **")
+print("    ** **          **      **   ***")
+print("     **         ********   **    **")
